dc41a561ce2ad5fef9ab1899ff00de9c4f52c46c
ferdelgado77 <fdelgado7777@gmail.com>
2017-10-14 17:20:59 -0500

    Subida 10 OCTUBRE 01

---
+++ app/src/main/java/solucionesbeacon/omall/items/Cupon.java
@@ -0,0 +1,35 @@
+package solucionesbeacon.omall.items;
+
+/**
+ * Created by Fernando Delgado on 14/10/2017.
+ */
+
+public class Cupon {
+    public  Integer idCupon;
+    public  String txtEnc;
+    public   String txtDet;
+
+    public Cupon(Integer id, String content, String encabezado,String estado) {
+        this.idCupon = id;
+        this.txtEnc = encabezado  ;
+        this.txtDet = estado;
+    }
+    public void setIdCupon(Integer idCupon) {
+        this.idCupon = idCupon;
+    }
+    public void setTxtEnc(String textEnc) {
+        this.txtEnc = textEnc;
+    }
+    public void setTxtDet(String textDet) {
+        this.txtDet = textDet;
+    }
+    public Integer getIdCupon() {
+        return idCupon;
+    }
+    public String getTxtEnc() {
+        return txtEnc;
+    }
+    public String getTxtDet() {
+        return txtDet;
+    }
+}
